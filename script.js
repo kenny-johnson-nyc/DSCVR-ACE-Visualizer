fe85c6cf30043c3d5619ed76023d36a4b590a995
kenny-johnson-nyc <kenny.johnson.nyc@gmail.com>
2022-07-04 20:26:22 -0400

    fixed pulling too much data

--- script.js
+++ script.js
@@ -4,20 +4,26 @@
 // const x_gse = [];
 // const y_gse = [];
 // const z_gse = [];
-const aceData = [];
-const dscovrData = [];
-const dscovrBackgroundColor = [];
-const aceBackgroundColor = [];
+let aceData = [];
+let dscovrData = [];
+let dscovrBackgroundColor = [];
+let aceBackgroundColor = [];
+
+// SSCweb data for ACE and DSCOVR is resolution 720 = 12 minutes
+
+const minutesPerPoint = 12;
+let pointsPerWeek = 7 * 24 * (60/minutesPerPoint); 
+const millisPerMinute = 60 * 1000;
+
+// let sampleRate = 168 * 2;  // # of points in a sample period, e.g., 168 = 1 week 
+
+let weeksPerOrbit = 26;  // # of samples, e.g., 26 weeks = months = 1 orbit
 const E = 8;
-const sampleRate=168; // # of hours in a sample period, e.g., 168 = 1 week 
-const sampleCount = 26; // # of samples, e.g., 2000 weeks = 38.5 years
+
 //const sampleLength
-
-
-
 //bubbleChart Setup Block
 
-const data = {
+const chartData = {
   lables: ['DSCOVR', 'ACE', 'SUN'],
   datasets: [
     {
@@ -53,10 +59,10 @@ const data = {
 
 const config = {
   type: 'bubble',
-  data,
+  data: chartData,
   options: {
     animation: {
-      duration: 5000,
+      duration: 2000,
       easing: 'easeOutQuart',
 
 
@@ -117,15 +123,12 @@ const config = {
 
 };
 
-
 //bubbleChart init block
 
 const bubbleChart = new Chart(
   document.getElementById('bubbleChart'),
   config
 );
-
-
 
 function updateChart() {
   // console.log("updateChart: dscovr data[1] " + JSON.stringify(data.datasets[0].data[1]));
@@ -202,49 +205,41 @@ function processData(results) {
        // Sample rate in hours (1 week = 168 hours)
       
       // sampleRate = 48;
+    
 
       console.log("dscovr data points " + tempDscovr.length);
-
-      for (i = 0; i < sampleRate * sampleCount; i += sampleRate) { 
-        dscovrData.push(tempDscovr[i]);
-        time_tag.push(tempDscovr[i].time_tag);
-        source.push(tempDscovr[i].source);
-        x_gse.push(tempDscovr[i].x_gse);
-        y_gse.push(tempDscovr[i].y_gse);
-        z_gse.push(tempDscovr[i].z_gse);
-      }
-      for (i = 0; i < sampleRate * sampleCount; i += sampleRate) {
-        aceData.push(tempAce[i]);
-        time_tag.push(tempAce[i].time_tag);
-        source.push(tempAce[i].source);
-        x_gse.push(tempAce[i].x_gse);
-        y_gse.push(tempAce[i].y_gse);
-        z_gse.push(tempAce[i].z_gse);
-      }
-      // console.log(time_tag);
-      // console.log(source);
-      // console.log(x_gse);
-      // console.log(y_gse);
-      // console.log(z_gse);
+    dscovrData = subsample(tempDscovr);
+    aceData = subsample(tempAce);  
 
       loadData();
       updateChart();
     }
 
+function subsample(inputData) { 
+  let i;
+  let outputData = [];
+
+  console.log("only subsampling up to point # " + (pointsPerWeek * weeksPerOrbit) + ", total # of points " + inputData.length);
+  for (i = 0; i < pointsPerWeek * weeksPerOrbit; i += pointsPerWeek) { 
+    outputData.push(inputData[i]);
+    // console.log("i " + i);
+  }
+  return outputData;
+ }
 
 function bubbleFader(dataPoints, backgroundColors, colors, spaceCraft) {
-  // console.log("dataPoints.length " + dataPoints.length);
+  console.log("bubbleFader dataPoints.length " + dataPoints.length);
   let i;
   for (i = 0; i < dataPoints.length; i++) {
     let bubbleRadius = 0.1 + Math.abs((i - dataPoints.length)) ** E / (dataPoints.length) ** E;
     let d = { x: dataPoints[i].y_gse, y: dataPoints[i].z_gse, r: 6 };
-    data.datasets[spaceCraft].data.push(d);
+    chartData.datasets[spaceCraft].data.push(d);
 
     //console.log("alpha " + ((dataPoints.length-i)/ dataPoints.length));
     backgroundColors.push(colors + ((dataPoints.length - i) / dataPoints.length) + ")");
     //console.log("color[" + i + "] " + backgroundColors[backgroundColors.length-1]);
   }
-  data.datasets[spaceCraft].backgroundColors = backgroundColors;
+  chartData.datasets[spaceCraft].backgroundColors = backgroundColors;
 }
 function loadData() {
   bubbleFader(dscovrData, dscovrBackgroundColor, "rgba(0,195,255,", 0);
@@ -347,18 +342,13 @@ function skipDuplicates(input) {
    let startTime;
    function defineEndTime() {
     let end = endTime.getTime();
-    // 2000 sample periods * 168 hour/period * 60 minute/hour * 60 second/minute * 1000 millis/second
-    let offset = sampleCount*sampleRate*60*60*1000;  //convert hours to milliseconds. hours back in time.
+    let offset = weeksPerOrbit*pointsPerWeek*minutesPerPoint*millisPerMinute;  //convert hours to milliseconds. hours back in time.
    //  console.log("sampleCount apiscript " + sampleCount);
     let start = end-offset;
     startTime = new Date(start);
     console.log(end + " "+ offset + "ms " + start + " " + startTime);
    }
-   
-   
-     
-   
-   
+    
    /**
     * Called when the browser finished construction the DOM.  It makes an
     * SSC web service call to get the available observatories.  The
@@ -378,7 +368,15 @@ function skipDuplicates(input) {
          let sscUrl='https://sscweb.gsfc.nasa.gov/WS/sscr/2/locations/ace,dscovr/'+start+','+end+'/';
          console.log("sscurl " +sscUrl);
          $.get(sscUrl, displayObservatories, 'json');
+
+         let sscUrl2='https://sscweb.gsfc.nasa.gov/WS/sscr/2/observatories/';
+         console.log("sscurl2 " +sscUrl2);
+        //  $.get(sscUrl2, displayInfo, 'json');
      });
+
+     function displayInfo(params) {
+        console.log(JSON.stringify(params));
+     }
      
      function displayObservatories(params) {
            // console.log(params);
@@ -421,6 +419,10 @@ function skipDuplicates(input) {
           
             let ace = {};
             ace.time_tag =  params.Result.Data[1][0].Time[1][1];
+            let size =  params.Result.Data[1][0].Time[1].length;
+            console.log("dscovr bounds " + JSON.stringify(params.Result.Data[1][0].Time[1][1]) + " to  " + params.Result.Data[1][0].Time[1][size-1]);
+            console.log("dscovr adjacents " + JSON.stringify(params.Result.Data[1][0].Time[1][2]) + " to  " + params.Result.Data[1][0].Time[1][size-2]);
+
             ace.x_gse =  params.Result.Data[1][0].Coordinates[1][0].X[1];
             ace.y_gse =  params.Result.Data[1][0].Coordinates[1][0].Y[1];
             ace.z_gse =  params.Result.Data[1][0].Coordinates[1][0].Z[1];
@@ -429,26 +431,46 @@ function skipDuplicates(input) {
             }
 
             let dscovr = {};
+            dscovr.time_tag =  params.Result.Data[1][1].Time[1][1];
+            size =  params.Result.Data[1][1].Time[1].length;
+            console.log("dscovr bounds " + JSON.stringify(params.Result.Data[1][1].Time[1][1]) + " to  " + params.Result.Data[1][1].Time[1][size-1]);
+            console.log("dscovr adjacents " + JSON.stringify(params.Result.Data[1][1].Time[1][2]) + " to  " + params.Result.Data[1][1].Time[1][size-2]);
             dscovr.x_gse =  params.Result.Data[1][1].Coordinates[1][0].X[1];
             dscovr.y_gse =  params.Result.Data[1][1].Coordinates[1][0].Y[1];
             dscovr.z_gse =  params.Result.Data[1][1].Coordinates[1][0].Z[1];
             for (let i = 0; i < dscovr.x_gse.length; i++) { //push every point 
               dscovrData.push({source: 'dscovr', x_gse: dscovr.x_gse[i], z_gse: dscovr.z_gse[i], y_gse: dscovr.y_gse[i]});
             }
+            console.log("initial aceData.length " + aceData.length);
+            console.log("initial dscovrData.length " + dscovrData.length);
+      
+          // clean up the data and reverse the time order      
+            let tempAce = skipDuplicates(aceData);
+            let tempDscovr = skipDuplicates(dscovrData);
 
+            console.log("skipped dups aceData.length " + aceData.length);
+            console.log("skipped dups dscovrData.length " + dscovrData.length);
 
-          let coordsY =  params.Result.Data[1][0].Coordinates[1][0].X[1];
-          console.log("coordsY " + JSON.stringify(coordsY));
+          // let coordsY =  params.Result.Data[1][0].Coordinates[1][0].X[1];
+          // console.log("coordsY " + JSON.stringify(coordsY));
+        
+          // subsample the data to improve the rendering performance 
+          aceData = subsample(tempAce);  
+          dscovrData = subsample(tempDscovr);
 
-          skipDuplicates(aceData);
-          skipDuplicates(dscovrData);
-
+          console.log("subsampled aceData.length " + aceData.length);
+          console.log("subsampled dscovrData.length " + dscovrData.length);
 
           loadData();
           updateChart();
      }
      
      function convertTime(time) {
+
+      console.log("hour " + zeroPad(time.getUTCHours()));
+      console.log("min " + zeroPad(time.getUTCMinutes()));
+      console.log("sec " + zeroPad(time.getUTCSeconds()));
+
       let d = '' + time.getUTCFullYear() + zeroPad(time.getUTCMonth()+1) + zeroPad(time.getUTCDate());
       let t =  "T" + zeroPad(time.getUTCHours()) + zeroPad(time.getUTCMinutes()) + zeroPad(time.getUTCSeconds()) + "Z";
       console.log("date " + d + " time " + t);
@@ -456,14 +478,11 @@ function skipDuplicates(input) {
       return '' + d + t; 
      }   
 
-
-
-
 /**
  * Zero Pad
  */
 function zeroPad(num){
-   return (num > 0 && num < 10) ? "0" + num : num; //between 0 and 10 add to num
+   return (num >= 0 && num < 10) ? "0" + num : num; //between 0 and 10 add to num
    }
 
 // function zeroPadTwo(num) {
